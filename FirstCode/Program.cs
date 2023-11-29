986e1174f7c686bad12357e23b95d60a5d5cbe8b
ejhirae <victor.osahon24@gmail.com>
2023-11-29 22:42:34 +0000

    Variables in C#

--- FirstCode/Program.cs
+++ FirstCode/Program.cs
@@ -2,6 +2,11 @@
 {
     private static void Main(string[] args)
     {
+        string bioName = "Victor";
+        int bioAge = 20;
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is " + bioName);
+        Console.WriteLine("I am "+ bioAge + " years old.");
+        Console.WriteLine("So that makes me a " + bioAge + " years old boy that is named " + bioName);
     }
 }
