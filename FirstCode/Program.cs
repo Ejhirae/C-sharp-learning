23253f34d75fda2859c6d9c42d99c595007de02f
ejhirae <victor.osahon24@gmail.com>
2023-11-29 01:59:43 +0000

    Creating a shape with  console write line

--- FirstCode/Program.cs
+++ FirstCode/Program.cs
@@ -3,5 +3,8 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        string age = Console.ReadLine();
+        Console.WriteLine(age);
+        Console.ReadLine();
     }
 }
