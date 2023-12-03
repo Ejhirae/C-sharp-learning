5512fdc2e786ec76d80d2c785fc640fb45fb058a
ejhirae <victor.osahon24@gmail.com>
2023-12-03 13:40:46 +0000

    for loops- normal for loop and foreach

--- FirstCode/Program.cs
+++ FirstCode/Program.cs
@@ -2,6 +2,18 @@
 {
     private static void Main(string[] args)
     {
+        string[] names = { "Victor", "Edward", "Amarachi", "Kitan" };
 
+        //For Normal
+        for(int i = names.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(names[i]);
+        }
+
+        //For each
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
     }
 }
