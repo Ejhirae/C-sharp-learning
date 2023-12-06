3ee26124697e3b97de4f9effbe1cd7e961a7f0fb
ejhirae <victor.osahon24@gmail.com>
2023-12-06 11:06:27 +0000

    testing getLength

--- FirstCode/Program.cs
+++ FirstCode/Program.cs
@@ -2,6 +2,31 @@
 {
     private static void Main(string[] args)
     {
+        //Student Age - Student Scores
 
+        int[,] studentDetails =
+        {
+            { 18, 50 },
+            { 23, 73},
+            { 54, 98},
+        };
+        try
+        {
+
+
+            for (int i = 0; i <= studentDetails.GetLength(0); i++)
+            {
+                Console.WriteLine(i);
+                for (int j = 0; j <= studentDetails.GetLength(0); j++)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine(studentDetails[i, j]);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
